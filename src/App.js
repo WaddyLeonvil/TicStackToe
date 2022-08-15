51fa32947f691562aaf218e258bc7221bb441c5f
WaddyLeonvil <54086752+WaddyLeonvil@users.noreply.github.com>
2022-08-15 13:29:26 -0400

    Added server

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React, {useState, useEffect} from 'react'; 
 import './App.css';
+import { w3cwebsocket } from 'websocket';
 import Checkers from './components/Checkers';
 import TicTacToe from './components/TicTacToe';
 
+const client = new w3cwebsocket('ws://127.0.0.1:8000');
+
 function App() {
+
+  useEffect(() => {
+     client.onopen = () => { 
+      console.log('WebSocket client connected.');
+     };
+     client.onmessage = (message) => {
+      const data = JSON.parse(message.data);
+      console.log('Got reply! ', data);
+     }
+  }, []);
+
   const [classic, setClassic] = useState(true);
 
   const handleButtonToggle = ({target}) => {
@@ -15,9 +29,17 @@ function App() {
     }
   }
 
+  const onButtonClicked = (message) => {
+    client.send(JSON.stringify({
+      type: 'message',
+      msg: message
+    }));
+  }
+
   return (
     <div className="App">
       <div className="page-header">
+        <button onClick={() => onButtonClicked("Hello")}>Send message</button>
         <div className="toggle">
           <button className={classic ? 'classic active' : 'classic'} onClick={handleButtonToggle} >
             Tic-Tac-Toe
---
+++ src/Server.js
@@ -0,0 +1,38 @@
+const webSocketsServerPort = 8000;
+const webSocketServer = require('websocket').server;
+const http = require('http');
+
+const server = http.createServer();
+server.listen(webSocketsServerPort);
+console.log('Listening on port 8000');
+
+const webServer = new webSocketServer({
+    httpServer: server
+})
+
+const clients = {};
+
+const getID = () => {
+    const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    return s4() + s4() + '-' + s4();
+}
+
+webServer.on('request', function(request) {
+    var userID = getID();
+    console.log((new Date()) + ' Received a new connection from origin ' + request.origin + '.');
+
+    const connection = request.accept(null, request.origin);
+    clients[userID] = connection;
+    console.log('Connected: ' + userID + ' in ' + Object.getOwnPropertyNames(clients));
+
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log('Received message: ', message.utf8Data);
+
+            for (let key in clients) {
+                clients[key].sendUTF(message.utf8Data);
+                console.log('Sent message to: ', clients[key]);
+            }
+        }
+    })
+})
