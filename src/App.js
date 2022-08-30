bf5de658a0199ed91c7c30ef8be90c6eb2370da1
WaddyLeonvil <54086752+WaddyLeonvil@users.noreply.github.com>
2022-08-30 15:52:23 -0400

    Altered log-in
    Removed messages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'; 
+import React, { useState, useEffect, useRef } from 'react'; 
 import './App.css';
 import 'antd/dist/antd.css';
 import { w3cwebsocket } from 'websocket';
@@ -29,7 +29,7 @@ function App() {
         }])
       }
      }
-  });
+  }, []);
 
   const [classic, setClassic] = useState(true);
 
@@ -56,30 +56,30 @@ function App() {
 
   return (
     <div className="App">
-      <div className="page-header">
-        {isLoggedIn ?
+      {isLoggedIn ?
         <>
-          <button className='msg-btn' onClick={() => onButtonClicked("Hello")}>Send message</button>
-          <button className='msg-btn' onClick={clearMessages}>Clear messages</button>
+          <div className="page-header">
+            <div className="toggle">
+              <button className={classic ? 'classic active' : 'classic'} onClick={handleButtonToggle} >
+                Tic-Tac-Toe
+              </button>
+              <button className={!classic ? 'checkers active' : 'checkers'} onClick={handleButtonToggle} >
+                Checkers
+              </button>
+            </div>
+          </div>
+          <div className="user-msgs">{messages.map(msg => <p className='user-msg'>Message: {msg.msg}, User: {msg.user}</p>)}</div>
+          <header className="App-header">
+            {classic ? <TicTacToe /> : <Checkers />}
+          </header>
         </>
         :
-        <div>
-          <Search placeholder='Enter Username' enterButton='Login' size='large' onSearch={value => {setIsLoggedIn(true); setUsername(value)}} />
+        <div className="App-header">
+          <div className='username'>
+            <Search placeholder='Enter Username' enterButton='Login' size='large' onSearch={value => {setIsLoggedIn(true); setUsername(value)}} />
+          </div>
         </div>
-        }
-        <div className="toggle">
-          <button className={classic ? 'classic active' : 'classic'} onClick={handleButtonToggle} >
-            Tic-Tac-Toe
-          </button>
-          <button className={!classic ? 'checkers active' : 'checkers'} onClick={handleButtonToggle} >
-            Checkers
-          </button>
-        </div>
-      </div>
-      <div className="user-msgs">{messages.map(msg => <p className='user-msg'>Message: {msg.msg}, User: {msg.user}</p>)}</div>
-      <header className="App-header">
-        {classic ? <TicTacToe /> : <Checkers />}
-      </header>
+      }
     </div>
   );
 }
